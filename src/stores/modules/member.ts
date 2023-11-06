c98616ed72eed8561c4dd88158396b149eca7b74
王鑫 <wb_wangxin06@kuaishou.com>
2023-11-06 16:30:13 +0800

    登录模块

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -1,42 +1,44 @@
+import type { LoginResult } from '@/types/member'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 // 定义 Store
 export const useMemberStore = defineStore(
-  'member',
-  () => {
-    // 会员信息
-    const profile = ref<any>()
+	'member',
+	() => {
+		// 会员信息
+		const profile = ref<LoginResult>()
 
-    // 保存会员信息，登录时使用
-    const setProfile = (val: any) => {
-      profile.value = val
-    }
+		// 保存会员信息，登录时使用
+		const setProfile = (val: LoginResult) => {
+			profile.value = val
+		}
 
-    // 清理会员信息，退出时使用
-    const clearProfile = () => {
-      profile.value = undefined
-    }
+		// 清理会员信息，退出时使用
+		const clearProfile = () => {
+			profile.value = undefined
+		}
 
-    // 记得 return
-    return {
-      profile,
-      setProfile,
-      clearProfile,
-    }
-  },
-  // 持久化
-  {
-    persist: {
-      // 调整为兼容多端的API
-      storage: {
-        setItem(key, value) {
-          uni.setStorageSync(key, value)
-        },
-        getItem(key) {
-          return uni.getStorageSync(key)
-        },
-      },
-    },
-  },
+		// 记得 return
+		return {
+			profile,
+			setProfile,
+			clearProfile,
+		}
+	},
+	{
+		// 网页端配置
+		// persist: true,
+		// 小程序端配置
+		persist: {
+			storage: {
+				getItem(key) {
+					return uni.getStorageSync(key)
+				},
+				setItem(key, value) {
+					uni.setStorageSync(key, value)
+				},
+			},
+		},
+	},
 )
